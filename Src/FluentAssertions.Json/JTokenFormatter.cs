33c9a0dbd06590b0fb5d02109c5a259b350c0fb0
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2021-08-14 14:15:50 +0200

    Add support for Fluent Assertions 6 (#55)

--- Src/FluentAssertions.Json/JTokenFormatter.cs
+++ Src/FluentAssertions.Json/JTokenFormatter.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using FluentAssertions.Formatting;
 using FluentAssertions.Json.Common;
 using Newtonsoft.Json.Linq;
@@ -22,26 +21,26 @@ namespace FluentAssertions.Json
             return value is JToken;
         }
 
-        /// <summary>
-        /// Returns a <see cref="string" /> that represents this instance.
-        /// </summary>
-        /// <param name="value">The value for which to create a <see cref="string"/>.</param>
-        /// <param name="useLineBreaks"> </param>
-        /// <param name="processedObjects">
-        /// A collection of objects that 
-        /// </param>
-        /// <param name="nestedPropertyLevel">
-        /// The level of nesting for the supplied value. This is used for indenting the format string for objects that have
-        /// no <see cref="object.ToString()"/> override.
-        /// </param>
-        /// <returns>
-        /// A <see cref="string" /> that represents this instance.
-        /// </returns>
-        public string Format(object value, FormattingContext context, FormatChild formatChild)
+        public void Format(object value, FormattedObjectGraph formattedGraph, FormattingContext context, FormatChild formatChild)
         {
-            var jToken = (JToken)value;
-            string result = context.UseLineBreaks ? jToken?.ToString(Newtonsoft.Json.Formatting.Indented) : jToken?.ToString().RemoveNewLines();
-            return result ?? "<null>";
+            var jToken = value as JToken;
+                
+            if (context.UseLineBreaks)
+            {
+                var result = jToken?.ToString(Newtonsoft.Json.Formatting.Indented);
+                if (result is not null)
+                {
+                    formattedGraph.AddFragmentOnNewLine(result);
+                }
+                else
+                {
+                    formattedGraph.AddFragment("<null>");    
+                }
+            }
+            else
+            {
+                formattedGraph.AddFragment(jToken?.ToString().RemoveNewLines() ?? "<null>");
+            }
         }
     }
 }
--- Tests/FluentAssertions.Json.Specs/JTokenFormatterSpecs.cs
+++ Tests/FluentAssertions.Json.Specs/JTokenFormatterSpecs.cs
@@ -7,48 +7,24 @@ namespace FluentAssertions.Json.Specs
     // ReSharper disable InconsistentNaming
     public class JTokenFormatterSpecs
     {
+        public JTokenFormatterSpecs()
+        {
+            Formatter.AddFormatter(new JTokenFormatter());
+        }
+
         [Fact]
         public void Should_Handle_JToken()
         {
             //-----------------------------------------------------------------------------------------------------------
-            // Arrange
+            // Act / Arrange
             //-----------------------------------------------------------------------------------------------------------
-            var sut = new JTokenFormatter();
-
-            //-----------------------------------------------------------------------------------------------------------
-            // Act
-            //-----------------------------------------------------------------------------------------------------------
-            var actual = sut.CanHandle(JToken.Parse("{}"));
+            var actual = Formatter.ToString(JToken.Parse("{}"));
             
             //-----------------------------------------------------------------------------------------------------------
             // Assert
             //-----------------------------------------------------------------------------------------------------------
-            actual.Should().BeTrue();
+            actual.Should().Be("{}");
         }
-
-        [Fact]
-        public void Should_not_handle_anything_else()
-        {
-            //-----------------------------------------------------------------------------------------------------------
-            // Arrange
-            //-----------------------------------------------------------------------------------------------------------
-            var testCases = new object[] { null, string.Empty };
-            var sut = new JTokenFormatter();
-
-            foreach (var testCase in testCases)
-            {
-                //-----------------------------------------------------------------------------------------------------------
-                // Act
-                //-----------------------------------------------------------------------------------------------------------
-                var actual = sut.CanHandle(testCase);
-
-                //-----------------------------------------------------------------------------------------------------------
-                // Assert
-                //-----------------------------------------------------------------------------------------------------------
-                actual.Should().BeFalse();
-            }
-        }
-
 
         [Fact]
         public void Should_preserve_indenting()
@@ -57,12 +33,14 @@ namespace FluentAssertions.Json.Specs
             // Arrange
             //-----------------------------------------------------------------------------------------------------------
             var json = JToken.Parse("{ \"id\":1 }");
-            var sut = new JTokenFormatter();
 
             //-----------------------------------------------------------------------------------------------------------
             // Act
             //-----------------------------------------------------------------------------------------------------------
-            var actual = sut.Format(json, new FormattingContext{UseLineBreaks = true}, (path, value) => "");
+            var actual = Formatter.ToString(json, new FormattingOptions
+            {
+                UseLineBreaks = true
+            });
 
             //-----------------------------------------------------------------------------------------------------------
             // Assert
@@ -77,13 +55,15 @@ namespace FluentAssertions.Json.Specs
             // Arrange
             //-----------------------------------------------------------------------------------------------------------
             var json = JToken.Parse("{ \"id\":1 }");
-            var sut = new JTokenFormatter();
 
             //-----------------------------------------------------------------------------------------------------------
             // Act
             //-----------------------------------------------------------------------------------------------------------
             // ReSharper disable once RedundantArgumentDefaultValue
-            var actual = sut.Format(json, new FormattingContext{UseLineBreaks = false}, (path, value) => "");
+            var actual = Formatter.ToString(json, new FormattingOptions
+            {
+                UseLineBreaks = false
+            });
             
             //-----------------------------------------------------------------------------------------------------------
             // Assert
