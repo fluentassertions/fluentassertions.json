1529c5d3db66925ddf33024e69972b54a6fa957b
souhailelk <elkaissi.souhail.me@gmail.com>
2022-02-01 17:27:55 +0100

    refactoring

--- Src/FluentAssertions.Json/IJsonAssertionOptions.cs
+++ Src/FluentAssertions.Json/IJsonAssertionOptions.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using FluentAssertions.Equivalency;
 
 namespace FluentAssertions.Json
 {
     public interface IJsonAssertionOptions<T>
     {
-        SelfReferenceEquivalencyAssertionOptions<EquivalencyAssertionOptions<T>>.Restriction<TProperty> Using<TProperty>(Action<IAssertionContext<TProperty>> action);
+        EquivalencyAssertionOptions<T>.Restriction<TProperty> Using<TProperty>(Action<IAssertionContext<TProperty>> action);
     }
 }
