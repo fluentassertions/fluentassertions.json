5c95025ba1459fbdfb30e7e62aa652003eeb21cc
Jonas Nyrup <jnyrup@gmail.com>
2022-02-24 18:59:19 +0100

    Remove IndexOfFirstMismatch

--- Src/FluentAssertions.Json/Common/StringExtensions.cs
+++ Src/FluentAssertions.Json/Common/StringExtensions.cs
@@ -1,26 +1,7 @@
-﻿using System;
-
-namespace FluentAssertions.Json.Common
+﻿namespace FluentAssertions.Json.Common
 {
     internal static class StringExtensions
     {
-        /// <summary>
-        /// Finds the first index at which the <paramref name="value"/> does not match the <paramref name="expected"/>
-        /// string anymore, accounting for the specified <paramref name="stringComparison"/>.
-        /// </summary>
-        public static int IndexOfFirstMismatch(this string value, string expected, StringComparison stringComparison)
-        {
-            for (int index = 0; index < value.Length; index++)
-            {
-                if ((index >= expected.Length) || !value[index].ToString().Equals(expected[index].ToString(), stringComparison))
-                {
-                    return index;
-                }
-            }
-
-            return -1;
-        }
-
         /// <summary>
         /// Replaces all characters that might conflict with formatting placeholders with their escaped counterparts.
         /// </summary>
