ec7dd185ba9348fde7d3f9aabd7c21c542280a97
makszimin <zim.ma2010@yandex.ru>
2025-10-03 18:17:21 +0200

    using TheoryData + adding JTokenComparer instead of compare like ToString

--- Src/FluentAssertions.Json/Common/JTokenExtensions.cs
+++ Src/FluentAssertions.Json/Common/JTokenExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json.Linq;
 
@@ -5,6 +7,8 @@ namespace FluentAssertions.Json.Common
 {
     internal static class JTokenExtensions
     {
+        private static readonly JTokenComparer Comparer = new();
+
         /// <summary>
         /// Recursively sorts the properties of JObject instances by name and
         /// the elements of JArray instances by their string representation,
@@ -15,9 +19,79 @@ namespace FluentAssertions.Json.Common
             return token switch
             {
                 JObject obj => new JObject(obj.Properties().OrderBy(p => p.Name).Select(p => new JProperty(p.Name, Normalize(p.Value)))),
-                JArray array => new JArray(array.Select(Normalize).OrderBy(x => x.ToString(Newtonsoft.Json.Formatting.None))),
+                JArray array => new JArray(array.Select(Normalize).OrderBy(x => x, Comparer)),
                 _ => token
             };
+        }
+
+        private sealed class JTokenComparer : IComparer<JToken>
+        {
+            public int Compare(JToken x, JToken y)
+            {
+                if (ReferenceEquals(x, y))
+                    return 0;
+
+                if (x is null)
+                    return -1;
+
+                if (y is null)
+                    return 1;
+
+                var typeComparison = x.Type.CompareTo(y.Type);
+                if (typeComparison != 0)
+                    return typeComparison;
+
+                return x switch
+                {
+                    JArray a => Compare(a, (JArray)y),
+                    JObject o => Compare(o, (JObject)y),
+                    JProperty p => Compare(p, (JProperty)y),
+                    JValue v => Compare(v, (JValue)y),
+                    _ => string.Compare(x.ToString(), y.ToString(), StringComparison.Ordinal)
+                };
+            }
+
+            private static int Compare(JValue x, JValue y) => Comparer<object>.Default.Compare(x.Value, y.Value);
+
+            private static int Compare(JArray x, JArray y)
+            {
+                var countComparison = x.Count.CompareTo(y.Count);
+                if (countComparison != 0)
+                    return countComparison;
+
+                return x
+                    .Select((t, i) => Comparer.Compare(t, y[i]))
+                    .FirstOrDefault(itemComparison => itemComparison != 0);
+            }
+
+            private static int Compare(JObject x, JObject y)
+            {
+                var countComparison = x.Count.CompareTo(y.Count);
+                if (countComparison != 0)
+                    return countComparison;
+
+                var xProperties = x.Properties().OrderBy(p => p.Name).ToArray();
+                var yProperties = y.Properties().OrderBy(p => p.Name).ToArray();
+
+                for (var i = 0; i < xProperties.Length; i++)
+                {
+                    var nameComparison = string.Compare(xProperties[i].Name, yProperties[i].Name, StringComparison.Ordinal);
+                    if (nameComparison != 0)
+                        return nameComparison;
+
+                    var valueComparison = Comparer.Compare(xProperties[i].Value, yProperties[i].Value);
+                    if (valueComparison != 0)
+                        return valueComparison;
+                }
+
+                return 0;
+            }
+
+            private static int Compare(JProperty x, JProperty y)
+            {
+                var nameComparison = string.Compare(x.Name, y.Name, StringComparison.Ordinal);
+                return nameComparison != 0 ? nameComparison : Comparer.Compare(x.Value, y.Value);
+            }
         }
     }
 }
--- Tests/FluentAssertions.Json.Specs/WithoutStrictOrderingSpecs.cs
+++ Tests/FluentAssertions.Json.Specs/WithoutStrictOrderingSpecs.cs
@@ -22,21 +22,22 @@ namespace FluentAssertions.Json.Specs
             // Assert
         }
 
-        public static IEnumerable<object[]> When_ignoring_ordering_BeEquivalentTo_should_succeed_sample_data()
+        public static TheoryData<string, string> When_ignoring_ordering_BeEquivalentTo_should_succeed_sample_data()
         {
-            yield return new object[] { @"{""ids"":[1,2,3]}", @"{""ids"":[3,2,1]}" };
-            yield return new object[] { @"{""ids"":[1,2,3]}", @"{""ids"":[1,2,3]}" };
-            yield return new object[] { @"{""type"":2,""name"":""b""}", @"{""name"":""b"",""type"":2}" };
-            yield return new object[] { @"{""names"":[""a"",""b""]}", @"{""names"":[""b"",""a""]}" };
-            yield return new object[]
+            return new TheoryData<string, string>
             {
-                @"{""vals"":[{""type"":1,""name"":""a""},{""name"":""b"",""type"":2}]}",
-                @"{""vals"":[{""type"":2,""name"":""b""},{""name"":""a"",""type"":1}]}"
-            };
-            yield return new object[]
-            {
-                @"{""vals"":[{""type"":1,""name"":""a""},{""name"":""b"",""type"":2}]}",
-                @"{""vals"":[{""name"":""a"",""type"":1},{""type"":2,""name"":""b""}]}"
+                { @"{""ids"":[1,2,3]}", @"{""ids"":[3,2,1]}" },
+                { @"{""ids"":[1,2,3]}", @"{""ids"":[1,2,3]}" },
+                { @"{""type"":2,""name"":""b""}", @"{""name"":""b"",""type"":2}" },
+                { @"{""names"":[""a"",""b""]}", @"{""names"":[""b"",""a""]}" },
+                {
+                    @"{""vals"":[{""type"":1,""name"":""a""},{""name"":""b"",""type"":2}]}",
+                    @"{""vals"":[{""type"":2,""name"":""b""},{""name"":""a"",""type"":1}]}"
+                },
+                {
+                    @"{""vals"":[{""type"":1,""name"":""a""},{""name"":""b"",""type"":2}]}",
+                    @"{""vals"":[{""name"":""a"",""type"":1},{""type"":2,""name"":""b""}]}"
+                }
             };
         }
 
@@ -55,14 +56,16 @@ namespace FluentAssertions.Json.Specs
             action.Should().Throw<XunitException>();
         }
 
-        public static IEnumerable<object[]> When_not_ignoring_ordering_BeEquivalentTo_should_throw_sample_data()
+        public static TheoryData<string, string> When_not_ignoring_ordering_BeEquivalentTo_should_throw_sample_data()
         {
-            yield return new object[] { @"{""ids"":[1,2,3]}", @"{""ids"":[3,2,1]}" };
-            yield return new object[] { @"{""names"":[""a"",""b""]}", @"{""names"":[""b"",""a""]}" };
-            yield return new object[]
+            return new TheoryData<string, string>
             {
-                @"{""vals"":[{""type"":1,""name"":""a""},{""name"":""b"",""type"":2}]}",
-                @"{""vals"":[{""type"":2,""name"":""b""},{""name"":""a"",""type"":1}]}"
+                { @"{""ids"":[1,2,3]}", @"{""ids"":[3,2,1]}" },
+                { @"{""names"":[""a"",""b""]}", @"{""names"":[""b"",""a""]}" },
+                {
+                    @"{""vals"":[{""type"":1,""name"":""a""},{""name"":""b"",""type"":2}]}",
+                    @"{""vals"":[{""type"":2,""name"":""b""},{""name"":""a"",""type"":1}]}"
+                }
             };
         }
     }
