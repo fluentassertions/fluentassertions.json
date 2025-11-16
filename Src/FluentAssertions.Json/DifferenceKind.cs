896423a7183c9cdde5b0124566183eda83e4598b
Jonas Nyrup <jnyrup@gmail.com>
2025-11-16 11:33:30 +0100

    Move types to separate files
    MA0048 - File name must match type name

---
+++ Src/FluentAssertions.Json/DifferenceKind.cs
@@ -0,0 +1,16 @@
+﻿namespace FluentAssertions.Json
+{
+    internal enum DifferenceKind
+    {
+        ActualIsNull,
+        ExpectedIsNull,
+        OtherType,
+        OtherName,
+        OtherValue,
+        DifferentLength,
+        ActualMissesProperty,
+        ExpectedMissesProperty,
+        ActualMissesElement,
+        WrongOrder
+    }
+}
---
+++ Src/FluentAssertions.Json/JPath.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace FluentAssertions.Json
+{
+    internal class JPath
+    {
+        private readonly List<string> nodes = new();
+
+        public JPath()
+        {
+            nodes.Add("$");
+        }
+
+        private JPath(JPath existingPath, string extraNode)
+        {
+            nodes.AddRange(existingPath.nodes);
+            nodes.Add(extraNode);
+        }
+
+        public JPath AddProperty(string name)
+        {
+            return new JPath(this, $".{name}");
+        }
+
+        public JPath AddIndex(int index)
+        {
+            return new JPath(this, $"[{index}]");
+        }
+
+        public override string ToString()
+        {
+            return string.Concat(nodes);
+        }
+    }
+}
